54ebe8b48a1fc6c31806da86d1489c0537aef4a6
Forever <forever@Forever.Forever>
2023-12-03 21:48:46 +0500

    add database mongoDB

--- src/server.js
+++ src/server.js
@@ -116,6 +116,35 @@ app.delete("/api/hero/:id", (req, res) => {
     });
 });
 
+app.put("/api/hero/:id", (req, res) => {
+  const blogId = req.params.id;
+  const { title, description, image } = req.body;
+
+  // Check if at least one field is provided in the request body
+  if (!title && !description && !image) {
+    return res.status(400).json({ error: "No data provided for update." });
+  }
+
+  // Construct the update object with provided fields
+  const updateObject = {};
+  if (title) updateObject.title = title;
+  if (description) updateObject.description = description;
+  if (image) updateObject.image = image;
+
+  // Update the blog post in the database
+  Blog.findByIdAndUpdate(blogId, updateObject, { new: true })
+    .then((updatedBlog) => {
+      if (!updatedBlog) {
+        return res.status(404).json({ error: "Blog post not found." });
+      }
+      res.json(updatedBlog);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error." });
+    });
+});
+
 connect();
 
 app.use("/uploads", express.static("uploads"));

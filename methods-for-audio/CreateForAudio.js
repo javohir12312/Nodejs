5f932e054c474b31fa2b5dd89da4c19155c9c878
Forever <forever@Forever.Forever>
2023-12-04 15:09:31 +0500

    add database mongoDB 2 day

--- methods-for-audio/CreateForAudio.js
+++ methods-for-audio/CreateForAudio.js
@@ -1,47 +1,39 @@
 const AudioSchema = require("../model/audio");
 
-
 module.exports = function createAudio(req, res) {
   const { title, number } = req.body;
   const audios = req.files['audio'];
-  const image = req.files['image'];
+  const images = req.files['image'];
 
-if (!image || image.length === 0) {
-  return res.status(400).json({ error: "No images uploaded for the 'image' field." });
-}
-
-const firstImage = image[0];
-
-  if (!title || !number || !audios || !image) {
+  if (!title || !number || !audios || !images || images.length === 0) {
     return res.status(400).json({ error: "Missing required fields for creating a new blog post." });
   }
 
-  const audioPaths = audios.map(audio => `/audio-uploads/${audio.filename}`);
+  const audioPaths = audios.map(audio => `/${audio.filename}`);
+  const firstImage = images[0];
 
   const newBlog = new AudioSchema({
     title,
     number,
     audios: audioPaths,
-    image: `${firstImage.path}`,
+    image: `${firstImage.filename}`, // Assuming filename is the correct property
   });
 
-  console.log(image+"xaxaxaxaxaxaxaxaxaxaxaxaxax");
-  console.log(image);
-  
-
+  // console.log("Image Path:", `/audi/${firstImage.filename}`);
+  console.log("Image Object:", firstImage);
 
   newBlog.save()
     .then((createdAudio) => {
       const blogWithUrls = {
         ...createdAudio._doc,
-        audios: createdAudio.audios.map(audio => `audio-uploads/${audio}`),
-        image: `/audio-uploads/${createdAudio.image}`,
+        audios: createdAudio.audios.map(audio => `${audio}`),
+        image: `${createdAudio.image}`,
       };
 
       res.status(201).json(blogWithUrls);
     })
     .catch((error) => {
       console.error(error);
-      res.status(500).json({ error: "Internal Server Error." });
+      res.status(500).json({ error: "Internal Server Error. Failed to save the audio entry." });
     });
 };

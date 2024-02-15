50b9d572b0aa6cbcc6c60ef015afc318721f3764
javohir12312 <abduxalilovjavohir393@gmail.com>
2024-02-15 09:02:21 +0500

    change cor

--- src/server.js
+++ src/server.js
@@ -122,14 +122,12 @@ app.post("/api/audios", uploadSmallAudio, CreateForAudio);
 
 const uploadAudio = multer({ storage: audioStorage, limits: { fileSize: 100000000 } });
 
-const config = {
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-  }
-};
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"] 
+}));
 
 app.use(express.json(config));
 

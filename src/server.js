a657c32593dc947c255a6ca566964af3299df6e1
javohir12312 <abduxalilovjavohir393@gmail.com>
2024-02-15 13:32:25 +0500

    change error

--- src/server.js
+++ src/server.js
@@ -184,6 +184,7 @@ app.post("/api/admin/confirm",SendConfirmationNumber)
 app.post("/api/admin/confirmation",confirmConfirmationCode)
 
 
+// Customers
 app.get("/api/customers",GetCuustomer)
 app.delete("/api/customers/:id",DeleteCustomer)
 app.post("/api/customers/",CreateCusmtomers)
@@ -191,20 +192,23 @@ app.post("/api/customers/login",loginCustomers)
 
 
 
+// Static file serving
 app.use("/uploads", express.static("uploads"));
 app.use("/uploads-logo", express.static("uploads-logo"));
 app.use("/audio-uploads", express.static("audio-uploads"));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+// Request logging middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
 });
 
+// Handle SIGINT signalxop 
 process.on('SIGINT', () => {
   mongoose.connection.close(() => {
     console.log('MongoDB connection closed.');
